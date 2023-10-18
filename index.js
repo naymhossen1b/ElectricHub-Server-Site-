2f136cb4ba363b73e1d4345a4cfa76c78d63e854
Naym Hossen <naym@Nayms-MBP.Dlink>
2023-10-18 11:59:25 +0600

    create a added route and create post data and get data function on server

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@firstpractice.poejscf.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -18,13 +16,29 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const userCollection = client.db("electricHub").collection("customarDB");
+
+
+
+    app.get('/products', async(req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/products', async(req, res) => {
+      const newData = req.body;
+      const result = await userCollection.insertOne(newData);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -35,11 +49,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('Electronic server is running')
-})
+app.get("/", (req, res) => {
+  res.send("Electronic server is running");
+});
 
 app.listen(port, () => {
-    console.log(`Eloctronic server port is running: ${port}`);
-})
+  console.log(`Eloctronic server port is running: ${port}`);
+});
